2247ccfa351f04eeb85f01ae7b89cc863424f122
Gilbert <gilbertzhang1208@gmail.com>
2022-05-27 06:44:13 -0400

    better env

--- env.d.ts
+++ env.d.ts
@@ -1,8 +1,11 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      // mysql://root:rebbit@localhost:3306/rebbit
       DATABASE_URL: string;
+      // $ openssl rand -base64 32
       NEXTAUTH_SECRET: string;
+      // http://localhost:3000
       NEXTAUTH_URL: string;
     }
   }
